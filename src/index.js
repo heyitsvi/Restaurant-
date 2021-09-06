3f2397289fa084551a81e4ed62a5ed0440e94c4a
Vivek <vivek99sanjeetsingh@gmail.com>
2021-09-06 21:36:06 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import { pageload } from "./pageload.js";
+pageload();
---
+++ src/pageload.js
@@ -0,0 +1,38 @@
+function pageload(){
+    const title = document.createElement('div');
+    title.textContent = "La Bel Restaurant";
+    title.setAttribute("id","heading");
+    console.log(document.body);
+    document.body.appendChild(title);
+
+    const tabs = document.createElement('div');
+    tabs.setAttribute("id","tabs");
+    document.querySelector("#content").appendChild(tabs);
+
+    const tab1 = document.createElement('button');
+    tab1.textContent = "About";
+    tab1.setAttribute("id","Btn1");
+    tab1.setAttribute("class","tabBtn");
+    const tab2 = document.createElement('button');
+    tab2.textContent = "Menu";
+    tab2.setAttribute("id","Btn2");
+    tab2.setAttribute("class","tabBtn");
+    const tab3 = document.createElement('button');
+    tab3.textContent = "Contact";
+    tab3.setAttribute("id","Btn3");
+    tab3.setAttribute("class","tabBtn");
+    document.querySelector("#tabs").appendChild(tab1);
+    document.querySelector("#tabs").appendChild(tab2);
+    document.querySelector("#tabs").appendChild(tab3);
+
+
+
+    const intro = document.createElement('div');
+    intro.textContent = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam quod similique, 
+    voluptas pariatur delectus odit maxime accusantium, dolores quae sequi explicabo adipisci 
+    totam perferendis harum neque? Est soluta laudantium ipsam.`;
+    intro.setAttribute("id","intro");
+    document.querySelector("#content").appendChild(intro);
+}
+
+export {pageload};
